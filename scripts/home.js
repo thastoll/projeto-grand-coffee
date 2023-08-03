2cf969a502f84a27cd616e0ff9341916983abac4
thastoll <thabatastoll@gmail.com>
2023-08-03 16:26:28 -0300

    primeiro commit

---
+++ scripts/home.js
@@ -0,0 +1,41 @@
+function menu(){
+    $(".button").click(function(){
+        $(".navContent").addClass("visible");
+    });
+}
+
+$(document).ready(function(){
+    // menu();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// function moreDetails(){
+//     $(".moreDetails").click(function(){
+//         $(".lightBox").removeClass("visible");
+
+//         let getLightBoxId = $(this).attr("data-id");
+//         $(getLightBoxId).addClass("visible");
+//     });
+//     closeDetails();
+// };
+
+// function closeDetails(){
+//     $(".btnClose").click(function(){
+//         $(".lightBox").removeClass("visible");
+//     });
+// };
+
+// $(document).ready(function(){
+//     moreDetails();
+// });
+
